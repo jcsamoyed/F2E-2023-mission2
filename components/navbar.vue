ce7d024ddd18bd7af79a3286cbfec554ef644b2b
jocelyn <jocelyn@ecloudmobile.com>
2023-11-21 11:35:50 +0800

    feat(layout): 新增預設佈局

---
+++ components/navbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <nav>
+    <div class="container">
+      <h1>2020 開票地圖</h1>
+    </div>
+  </nav>
+</template>
+
+<style lang="scss" scoped>
+nav {
+  background-color: #262E49;
+  color: #fff;
+  font-size: 32px;
+  font-weight: 600;
+  padding: 18px 0;
+}
+</style>
