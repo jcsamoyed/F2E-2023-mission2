51ef31f50056c12695852b96aa0dc651b0efc8aa
jcsamoyed <aruby5223@gmail.com>
2023-11-27 21:45:15 +0800

    build: 配置部署設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/F2E-2023-mission2/' : '/',
+    buildAssetsDir: '/static/',
+  },
   css: ['~/assets/css/main.scss'],
   build: {
     transpile: [/echarts/],
