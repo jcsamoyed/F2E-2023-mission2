f13512a039a9491c6ec433feed12ecf22a372802
jcsamoyed <aruby5223@gmail.com>
2023-11-27 21:56:17 +0800

    build: 新增 head 全域設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,17 @@ export default defineNuxtConfig({
   app: {
     baseURL: process.env.NODE_ENV === 'production' ? '/F2E-2023-mission2/' : '/',
     buildAssetsDir: '/static/',
+    head: {
+      title: '2020 開票地圖',
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: '多位候選人角逐台灣總統，究竟誰輸誰贏？' },
+        { property: 'og:title', content: '2020 開票地圖' },
+        { property: 'og:description', content: '多位候選人角逐台灣總統，究竟誰輸誰贏？' },
+        { property: 'og:image', content: 'https://jcsamoyed.github.io/F2E-2023-mission2/og-image.png' },
+        { property: 'og:url', content: 'https://jcsamoyed.github.io/F2E-2023-mission2' },
+      ],
+    },
   },
   css: ['~/assets/css/main.scss'],
   build: {
